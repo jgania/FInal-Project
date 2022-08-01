e6184893cc316aa681ac3827137a7a6363c7c265
jgania <jordan.gania@gmail.com>
2022-07-31 18:11:19 -0600

    Updates 7/31 mk 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from "./components/NavbarComp";
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   const [data, setData] = useState({})
@@ -23,43 +26,43 @@ function App() {
     <div className="app">
       <NavbarComp/>
       <div className="search">
-        <input
-          value={location}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
+          <input
+            value={location}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
       </div>
       <div className="container">
-        <div className="top">
-          <div className="location">
-            <p>{data.name}</p>
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
           </div>
-          <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
-          </div>
-          <div className="description">
-            {data.weather ? <p>{data.weather[0].main}</p> : null}
-          </div>
-        </div>
 
-        {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
-              <p>Feels Like</p>
+          {data.name !== undefined &&
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+                <p>Feels Like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                <p>Wind Speed</p>
+              </div>
             </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-              <p>Wind Speed</p>
-            </div>
-          </div>
-        }
-      </div>
+          }
+        </div>
     </div>
   );
 }
